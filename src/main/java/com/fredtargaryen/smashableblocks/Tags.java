599329b81b3821327fc707f691a526856163a43c
fredtargaryen <lggy@hotmail.co.uk>
2026-04-11 03:19:57 +0100

    Update to 1.21.11... somehow not pushed

--- src/main/java/com/fredtargaryen/smashableblocks/Tags.java
+++ src/main/java/com/fredtargaryen/smashableblocks/Tags.java
@@ -7,7 +7,7 @@ import net.minecraft.tags.TagKey;
 import net.minecraft.world.entity.EntityType;
 
 public class Tags {
-    public static final TagKey<EntityType<?>> EXCLUDED = TagKey.create(Registries.ENTITY_TYPE, DataReference.getResourceLocation("excluded"));
-    public static final TagKey<EntityType<?>> SMASHERS_LIGHT = TagKey.create(Registries.ENTITY_TYPE, DataReference.getResourceLocation("smashers_light"));
-    public static final TagKey<EntityType<?>> SMASHERS_HEAVY = TagKey.create(Registries.ENTITY_TYPE, DataReference.getResourceLocation("smashers_heavy"));
+    public static final TagKey<EntityType<?>> EXCLUDED = TagKey.create(Registries.ENTITY_TYPE, DataReference.getIdentifier("excluded"));
+    public static final TagKey<EntityType<?>> SMASHERS_LIGHT = TagKey.create(Registries.ENTITY_TYPE, DataReference.getIdentifier("smashers_light"));
+    public static final TagKey<EntityType<?>> SMASHERS_HEAVY = TagKey.create(Registries.ENTITY_TYPE, DataReference.getIdentifier("smashers_heavy"));
 }
